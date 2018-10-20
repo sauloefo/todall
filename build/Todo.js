3a0cb735dbbba8dd4acb58e9a1e655554c91d2d6
Saulo Oliveira <sauloeduardo@gmail.com>
2018-10-20 22:29:46 +0100

    Remove Build Folder

--- build/Todo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Todo = (function () {
-    function Todo(title) {
-        this.title = title;
-    }
-    return Todo;
-}());
-exports.default = Todo;
